b0d4830f6059cf470d2c42ce983cf84ab55af8e0
Ruslan Vasiliev <it.vasiliev@gmail.com>
2023-10-29 16:36:19 +0200

    commit hw4

--- hw4/script.js
+++ hw4/script.js
@@ -5,27 +5,30 @@
 
 const str = "Це довільний текст для створення масиву слів ";
 function stringToArray(str) {
-    // Ваш код
+  return  str.toLowerCase().trim().split(" ").sort()
 }
-
 console.log(stringToArray(str));    // виводить [ 'для', 'довільний', 'масиву', 'слів', 'створення', 'текст', 'це' ]
 
 /*
 2. Написати функцію, яка видаляє з масива елементи, що дублюються, та повертає масив оригінальних елементів,
 відсортованих за зростанням
  */
+
 const initialArray = [1, 4, 6, 6, 7, 5, 34, 5, 1000, 66, 1];
 function removeDuplicatesAndSort(arr) {
-    // Ваш код
+    let numbers = new Set(arr)
+    return Array.from(numbers).sort((a, b) => a - b)
 }
 
 console.log(removeDuplicatesAndSort(initialArray)); // виводить [ 1, 4, 5, 6, 7, 34, 66, 1000 ]
+
 /*
 3. Написати функцію, яка поверне масив парних чисел
  */
+
 const initialArray2 = [2, 3, 6, 7, 9, 12];
 function arrayOfEvens(arr) {
-    // Ваш код
+    return arr.filter((elem) => !(elem % 2))
 }
 
 console.log(arrayOfEvens(initialArray2)); // виводить [ 2, 6, 12 ]
@@ -35,9 +38,21 @@ console.log(arrayOfEvens(initialArray2)); // виводить [ 2, 6, 12 ]
 кожний наступний елемент послідовності на самого себе. Максимальне значення масиву не повинно перевищувати число 30.
  */
 function createArray(element) {
-    // Ваш код
+    let arr = [];
+    let number = element;
+
+    while (number <= 30) {
+        arr.push(number);
+        number += element;
+    }
+    return arr;
+
 }
 
 console.log(createArray(5)); // виведе [ 5, 10, 15, 20, 25, 30 ]
 console.log(createArray(3)); // виведе [ 3,  6,  9, 12, 15, 18, 21, 24, 27, 30 ]
+
+
+
+
 
