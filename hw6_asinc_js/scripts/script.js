cd577aa49f4550d4a18b934a80bb85e80e900b70
Ruslan Vasiliev <it.vasiliev@gmail.com>
2024-01-21 20:04:59 +0200

    commit hw-6

--- hw6_asinc_js/scripts/script.js
+++ hw6_asinc_js/scripts/script.js
@@ -4,13 +4,14 @@ import * as request from "./api_helper/api_helper.js";
 const baseUrl = "https://jsonplaceholder.typicode.com/posts/";
 
 // Task 1.
-const getPost = await request.getPosts(baseUrl)
+/*const getPost = await request.getPosts(baseUrl)
 console.log(getPost)
 
 const creatPost = await request.creatPosts(baseUrl)
-console.log(creatPost)
+console.log(creatPost)*/
 
 //Task 2.
+/*
 async function getPostsByUsedId(url, userId) {
     const data = await request.getPosts(url);
     const filteredData = data.filter((elem) => elem.userId === userId);
@@ -20,8 +21,10 @@ async function getPostsByUsedId(url, userId) {
 
 const posts = await getPostsByUsedId(baseUrl, 5);
 console.log(posts); // маємо тільки пости юзера з id = 5, у яких нема title
+*/
 
 // Task 3.
+/*
 async function createNewPost(url, body) {
     const response = await fetch((url), {
         method: "post",
@@ -34,24 +37,22 @@ async function createNewPost(url, body) {
 
 const result = await createNewPost(baseUrl, body);
 console.log(result); // повинен буди респонс у вигляді { userId: 155, title: 'New title', body: 'New body', id: 101 }
+*/
 
 // Task 4.
+
 function resolveNumber() {
-    let randomNumber = Math.floor(Math.random() * 10);
-    let promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-           if(randomNumber >= 5){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=> {
+            const randomNumber = Math.floor(Math.random() * 11);
+            if (randomNumber > 5) {
                 resolve(`Resolved ${randomNumber}`)
-        } else {
-            reject(`Rejected ${randomNumber}`);
-            }
-        }, 3000);
-    });
-    return promise.then(
-        resolve => console.log(resolve)
-    ).catch(
-        reject => console.log(reject)
-    )
+            } else (reject(`Rejected ${randomNumber}`))
+        }, 3000)
+    })
 }
 
+resolveNumber()
+    .then(result => {console.log(result)})
+    .catch(error => {console.log(error)})
 resolveNumber()//далі обробка промісу, в консолі або, наприклад, Resolved 7, або Rejected 2 (в залежності від рандомно створенного числа)
